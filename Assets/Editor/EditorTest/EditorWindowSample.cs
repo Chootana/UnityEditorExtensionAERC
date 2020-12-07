2a8c6640bb9e12534d6d8ad278ddbde28414274f
Chootana <t.anthony2074@gmail.com>
2020-12-07 22:52:56 +0900

    add copy component script v1

---
+++ Assets/Editor/EditorTest/EditorWindowSample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class EditorWindowSample : EditorWindow
+{
+    [MenuItem("Editor/Sample")]
+    private static void Create()
+    {
+        GetWindow<EditorWindowSample>("サンプル");
+    }
+}
